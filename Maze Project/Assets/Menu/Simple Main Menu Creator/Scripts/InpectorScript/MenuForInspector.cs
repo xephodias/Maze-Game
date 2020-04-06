2871ce4728d46dd1b844d09536add748d88d4630
xephodias <43187601+xephodias@users.noreply.github.com>
2020-04-06 12:57:37 -0600

    Added Menu and weather assets

---
+++ Maze Project/Assets/Menu/Simple Main Menu Creator/Scripts/InpectorScript/MenuForInspector.cs
@@ -0,0 +1,357 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+using UnityEditor;
+[System.Serializable]
+[CustomEditor(typeof(GUIThingsMenu))]
+[CanEditMultipleObjects]
+
+public class MenuForInspector : Editor {
+    //Variables
+    private bool UseColorText = false;
+    private bool UseTransperency = false;
+	private Vector2 scrollposition;
+	private Vector2 scrollpositionBox;
+	private Vector2 scrollpositionTextures;
+
+	void GUIStyleSettings(GUIStyle style, int fontSize, FontStyle fontsl, TextAnchor textanch) {
+        style.richText = true;
+		style.alignment = textanch;
+		style.fontStyle = fontsl;
+		style.fontSize = fontSize;
+    }
+
+	public override void OnInspectorGUI() {
+		BigSpace (1, false, 0);
+
+        GUIThingsMenu script = (GUIThingsMenu)target;
+		GUIStyle style = new GUIStyle();
+		GUIStyle mainstyle = new GUIStyle ();
+		GUIStyleSettings(style, 20, FontStyle.Italic, TextAnchor.MiddleCenter);
+		GUIStyleSettings (mainstyle, 20, FontStyle.Bold, TextAnchor.LowerCenter);
+
+		EditorGUILayout.LabelField (@"Main Menu Creator", mainstyle);
+
+		script.guiSkin = (GUISkin)EditorGUILayout.ObjectField("GUI skin: ", script.guiSkin, typeof(GUISkin), true);
+
+		EditorGUILayout.Space();
+		EditorGUILayout.LabelField("Components", style);
+
+		BigSpace(3,false,0);
+        
+        //Color settings
+		if(GUILayout.Button("Сolor of buttons")) {
+			UseColorText=true;
+			script.UseColorText=true;
+		}
+
+		if(UseColorText){
+			EditorGUILayout.BeginVertical("box"); {
+				EditorGUILayout.LabelField ("Color", style);
+				BigSpace (2, false, 0);
+				script.ColorText = EditorGUI.ColorField (GUILayoutUtility.GetRect (15, 15), script.ColorText);
+
+				if (GUILayout.Button ("Close Menu")) {
+					UseColorText = false;
+				}
+				EditorGUILayout.EndVertical ();
+			}
+		}
+
+		BigSpace(1,false,0);
+
+		//Transparency settings
+		if(GUILayout.Button("Transparency")) {
+			UseTransperency=true;
+			script.UseTransparancy = true;
+		}
+		if(UseTransperency==true) {
+			EditorGUILayout.BeginVertical("box"); {
+				EditorGUILayout.LabelField ("Transparency", style);
+				EditorGUILayout.Space ();
+
+				script.Transparency = EditorGUILayout.IntSlider ((int)script.Transparency, 0, 1000, null);
+				ProgressBar ((float)script.Transparency / 1000, "Transparency");
+
+				if (GUILayout.Button ("Close Menu")) {
+					UseTransperency = false;
+				}
+				EditorGUILayout.EndVertical ();
+			}
+		}
+
+		BigSpace(1,false,0);
+
+		if (GUILayout.Button ("Animations")) {
+			script.StartAnim_Interface = true;
+		}
+
+		if (script.StartAnim_Interface) {
+			EditorGUILayout.BeginVertical ("box"); {
+				EditorGUILayout.LabelField ("Start Animation:", style);
+				script.UseStartAnimation = GUILayout.Toggle (script.UseStartAnimation, "Use animation:");
+				if (script.UseStartAnimation) {
+					script.speedAnimation = EditorGUILayout.FloatField ("Speed:", script.speedAnimation);
+					EditorGUILayout.HelpBox (@" 1 - Very Slow. 
+				10 - Normal.
+				30 - Very fast.", MessageType.Info);
+				}
+				if (GUILayout.Button ("Close Menu")) {
+					script.StartAnim_Interface = false;
+				}
+				EditorGUILayout.EndVertical();
+			}
+		}
+
+		BigSpace (1, false, 0);
+
+		if (GUILayout.Button ("Preview Texture")) {
+			script.UsePreviewTexture_Interface = true;
+		}
+
+		if (script.UsePreviewTexture_Interface) {
+			EditorGUILayout.BeginVertical ("box"); {
+				EditorGUILayout.LabelField ("Preview Texture:", style);
+				script.UsePreviewTexture = GUILayout.Toggle (script.UsePreviewTexture, "Use Preview Texture:");
+				if (script.UsePreviewTexture) {
+					script.previewtexture = (Texture2D)EditorGUI.ObjectField(GUILayoutUtility.GetRect(15,15,""), "Texture:", script.previewtexture, typeof(Texture2D), true);
+					script.previewplacement = (GUIThingsMenu.PreviewTexturePlacement)EditorGUILayout.EnumPopup ("Placement:", script.previewplacement);
+					script.widthPreviewTexture = EditorGUILayout.IntField ("Width:", script.widthPreviewTexture);
+					script.heightPreviewTexture = EditorGUILayout.IntField ("Height:", script.heightPreviewTexture);
+				}
+
+				if (GUILayout.Button ("Close")) {
+					script.UsePreviewTexture_Interface = false;
+				}
+
+				EditorGUILayout.EndVertical ();
+			}
+		}
+
+		BigSpace(5,false,3);
+		
+        //Button system
+		EditorGUILayout.LabelField("Buttons", style);
+		BigSpace(2,false,0);
+
+        if (script.CurButtons < 15) {
+            if (GUILayout.Button("Add button")) {
+                script.CurButtons++;
+            }
+        }
+
+		script.UseCustomPosition = GUILayout.Toggle(script.UseCustomPosition, "Use Custom Position");
+
+        if (!script.UseCustomPosition) {
+            script.opt = (GUIThingsMenu.Option)EditorGUILayout.EnumPopup("Placement:", script.opt);
+        }
+
+		BigSpace(2, true, 1);
+    
+		if(script.CurButtons>=3)
+			scrollposition =
+				EditorGUILayout.BeginScrollView (scrollposition, false, true, GUILayout.Height (500));
+		
+		for (int i = 0; i < script.CurButtons; ++i) {
+            EditorGUILayout.BeginVertical("box"); {
+                EditorGUILayout.LabelField("Button " + Convert.ToString(i + 1), EditorStyles.boldLabel);
+                if (script.UseCustomPosition) {
+                    script.xPos[i] = EditorGUILayout.IntField("X position:", script.xPos[i]);
+                    script.yPos[i] = EditorGUILayout.IntField("Y position:", script.yPos[i]);
+                }
+                script.width[i] = EditorGUILayout.IntField("Width:", script.width[i]);
+                script.height[i] = EditorGUILayout.IntField("Height:", script.height[i]);
+
+                EditorGUILayout.Space();
+
+                script.labelsOfButtons[i] = EditorGUILayout.TextField("Label of Button:", script.labelsOfButtons[i]);
+                EditorGUILayout.Space();
+
+                script.UseCustomPlusPositions[i] = GUILayout.Toggle(script.UseCustomPlusPositions[i], "Add coordinates?");
+                if (script.UseCustomPlusPositions[i]){
+                    script.PlusX[i] = EditorGUILayout.IntField("Add X:", script.PlusX[i]);
+                    script.PlusY[i] = EditorGUILayout.IntField("Add Y:", script.PlusY[i]);
+                }
+
+                script.btnDo[i] = (GUIThingsMenu.ListOfDo)EditorGUILayout.EnumPopup("Function:", script.btnDo[i]);
+                if (script.btnDo[i] == GUIThingsMenu.ListOfDo.ChangeLevel) {
+                    script.SceneNumbers[i] = EditorGUILayout.IntField("Level Number:", script.SceneNumbers[i]);
+                }
+
+				if (script.btnDo [i] == GUIThingsMenu.ListOfDo.ChangePage) {
+					script.changebtnpage[i] = EditorGUILayout.IntField ("Change page to:", script.changebtnpage[i]);
+				}
+
+				if (script.btnDo [i] == GUIThingsMenu.ListOfDo.ChangeQuality) {
+					script.qualitynum [i] = EditorGUILayout.IntField ("Quality Level:", script.qualitynum[i]);
+				}
+
+                EditorGUILayout.Space();
+
+                script.IsPlaySound[i] = GUILayout.Toggle(script.IsPlaySound[i], "Play sound?");
+                if(script.IsPlaySound[i]) {
+					script.PlaySound[i] = (AudioClip)EditorGUILayout.ObjectField("Sound: ", script.PlaySound[i], typeof(AudioClip), true);
+                }
+
+				script.startbtnpage[i] = EditorGUILayout.IntField ("Page:", script.startbtnpage[i]);
+                EditorGUILayout.EndVertical();
+            }
+		}
+
+		if(script.CurButtons>=3)
+			EditorGUILayout.EndScrollView();   	 
+
+        if (script.CurButtons > 0) {
+			if(GUILayout.Button("Remove Button")) {
+			    script.CurButtons--;
+		    }
+		}
+
+		BigSpace(5,false,3);
+
+		//Box system
+
+		EditorGUILayout.LabelField("Boxes", style);
+		BigSpace(1,false,0);
+
+		script.UseCustomPosBox = GUILayout.Toggle(script.UseCustomPosBox, "Use Custom Position");
+		if(!script.UseCustomPosBox)  
+			script.boxopt = (GUIThingsMenu.BoxOption)EditorGUILayout.EnumPopup("Placement: ", script.boxopt);
+
+		if(script.CurBoxes < 10 && (script.boxopt == GUIThingsMenu.BoxOption.Center || script.UseCustomPosBox)) {
+			if(GUILayout.Button("Add Box")) {
+				script.CurBoxes++;
+			}
+		}
+
+		if (script.UseCustomPosBox)
+			script.boxopt = GUIThingsMenu.BoxOption.Center; 
+
+		if (script.boxopt != GUIThingsMenu.BoxOption.AboveTheAllComponents) {
+			
+			if(script.CurBoxes >= 4)
+				scrollpositionBox =
+					EditorGUILayout.BeginScrollView (scrollpositionBox, false, true, GUILayout.Height (300));
+
+			for(int i = 0; i < script.CurBoxes; ++i) {
+				EditorGUILayout.BeginVertical("box"); {
+					EditorGUILayout.LabelField("Box " + Convert.ToString(i + 1), EditorStyles.boldLabel);
+
+					if (script.UseCustomPosBox) {
+						script.XposBoxes[i] = EditorGUILayout.IntField("X position:", script.XposBoxes[i]);
+						script.YposBoxes[i] = EditorGUILayout.IntField("Y position:", script.YposBoxes[i]);
+					}
+
+					script.WidthBoxes[i] = EditorGUILayout.IntField("Width:", script.WidthBoxes[i]);
+					script.HeightBoxes[i] = EditorGUILayout.IntField("Height:", script.HeightBoxes[i]);
+
+					EditorGUILayout.Space();
+
+					script.textOfBoxes[i] = EditorGUILayout.TextField("Label of Box:", script.textOfBoxes[i]);
+					EditorGUILayout.EndVertical();
+				}
+			}
+
+			if(script.CurBoxes >= 4)
+				EditorGUILayout.EndScrollView ();
+		}
+
+		switch(script.boxopt) {
+		case GUIThingsMenu.BoxOption.AboveTheAllComponents:
+			int pages = -1000;
+			for (int i = 0; i < script.CurButtons; ++i) {
+				if (pages < script.startbtnpage[i])
+					pages = script.startbtnpage[i];
+			}
+
+			if (!script.UseCustomPosBox) {
+				for (int i = 0; i < pages + 1; ++i) {
+					script.boxName [i] = EditorGUILayout.TextField ("Page " + Convert.ToString (i + 1) + " title:", script.boxName [i]);
+				}
+				//script.textOfBoxes [0] = EditorGUILayout.TextField ("Label of Box:", script.textOfBoxes [0]);
+			}
+
+
+				break;
+		}
+		if (script.CurBoxes > 0 && (script.boxopt == GUIThingsMenu.BoxOption.Center || script.UseCustomPosBox)) {
+			if (script.CurBoxes >= 1) {
+				if (GUILayout.Button ("Remove Box")) {
+					script.CurBoxes--;
+				}
+			}
+		}
+
+		BigSpace(5,false,3);
+
+		//TextureSystem
+		EditorGUILayout.LabelField("Textures", style);
+		script.UseCustomPositionTexture = GUILayout.Toggle (script.UseCustomPositionTexture, "Use Custom Position:");
+		if (script.TextureCount < 10) {
+			if (GUILayout.Button ("Add Texture")) {
+				script.TextureCount++;
+			}
+		}
+
+
+		if(script.TextureCount>=3)
+			scrollpositionTextures =
+				EditorGUILayout.BeginScrollView (scrollpositionTextures, false, true, GUILayout.Height (450));
+		
+		for (int i = 0; i < script.TextureCount; ++i) {
+			EditorGUILayout.BeginVertical("box"); {
+				EditorGUILayout.LabelField("Texture " + Convert.ToString(i + 1), EditorStyles.boldLabel);
+
+				script.Textures [i] = (Texture2D)EditorGUI.ObjectField (GUILayoutUtility.GetRect (15, 15), "Texture: ", script.Textures[i], typeof(Texture2D), true);
+				if (script.UseCustomPositionTexture) {
+					script.XpositionTexture [i] = EditorGUILayout.IntField ("X position:", script.XpositionTexture [i]);
+					script.YpositionTexture [i] = EditorGUILayout.IntField ("Y position:", script.YpositionTexture [i]);
+				} else {
+					script.NearButtonTextureNum[i] = EditorGUILayout.IntField ("Button Number:", script.NearButtonTextureNum[i]);
+					if (script.NearButtonTextureNum[i] < 1 || script.NearButtonTextureNum[i] > script.CurButtons)
+						script.NearButtonTextureNum[i] = 1;
+
+					script.nearbtnplacement [i] = (GUIThingsMenu.TextureNearButtonPlacement)EditorGUILayout.EnumPopup ("Placement: ", script.nearbtnplacement [i]);
+				}
+
+				script.WidthTexture [i] = EditorGUILayout.IntField ("Width:", script.WidthTexture [i]);
+				script.HeightTexture [i] = EditorGUILayout.IntField ("Height:", script.HeightTexture [i]);
+
+				script.UseBoxBehind [i] = GUILayout.Toggle (script.UseBoxBehind [i], "Use Box behind");
+
+				EditorGUILayout.EndVertical();
+			}
+		}
+
+		if (script.TextureCount >= 3)
+			EditorGUILayout.EndScrollView ();
+
+		if (script.TextureCount != 0) {
+			if (GUILayout.Button ("Remove Texture")) {
+				script.TextureCount--;
+			}
+		}
+
+		BigSpace (3, false, 0);
+		GUILayout.Label ("Powered by FixThis studio");
+    }
+
+	void ProgressBar (float value, string text) {
+		Rect rect = GUILayoutUtility.GetRect (18, 18, "TextField");
+		EditorGUI.ProgressBar (rect, value, text);
+		EditorGUILayout.Space ();
+	}
+
+	void BigSpace (int value, bool UseLine, int size) {
+		for(int i = 0; i < value; ++i) {
+			EditorGUILayout.Space();
+			if (i == value/2) {
+				if(UseLine)
+					EditorGUI.DrawRect(GUILayoutUtility.GetRect(200,size), Color.black);
+			}
+		}
+	}
+}
+#endif 
---
+++ Maze Project/Assets/Menu/Simple Main Menu Creator/Scripts/MainScript/GUIThingsMenu.cs
@@ -0,0 +1,389 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using UnityEngine.SceneManagement;
+public class GUIThingsMenu : MonoBehaviour {
+	
+	//GUISkin
+	public GUISkin guiSkin = null;
+
+	//Color 
+	public Color ColorText;
+	public bool UseColorText;
+
+	//Transperency
+	public float Transparency;
+	public bool UseTransparancy;
+
+	//Buttons position
+	public int[] xPos = new int[30];
+	public int[] yPos = new int[30];
+	public int[] width = new int[30];
+	public int[] height = new int[30];
+
+	public int[] PlusX = new int[30];
+	public int[] PlusY = new int[30];
+
+	public Rect[] RectsOfButtons = new Rect[30];
+
+	//Custom settings (buttons)
+	public bool UseDefaultSettings;
+	public bool UseCustomSettings;
+	public bool UseCustomPosition;
+	public bool[] UseCustomPlusPositions = new bool[30];
+	public string[] labelsOfButtons = new string[30];
+	public int CurButtons;
+	public Texture2D[] OnHoverBtn = new Texture2D[30];
+	public int[] SceneNumbers = new int[30];
+	public bool[] IsPlaySound = new bool[30];
+	public AudioClip[] PlaySound = new AudioClip[30];
+	public int[] startbtnpage = new int[30];
+	public int[] changebtnpage = new int[30];
+	public int[] qualitynum = new int[30];
+	private int CurPage = 0;
+
+	//Textures
+	public Rect[] RectsOfTextures = new Rect[30];
+	public int TextureCount=1;
+	public Texture2D[] Textures = new Texture2D[30];
+	public bool UseCustomPositionTexture;
+	public int[] XpositionTexture = new int[30];
+	public int[] YpositionTexture = new int[30];
+	public int[] WidthTexture = new int[30];
+	public int[] HeightTexture = new int[30];
+	public bool[] UseBoxBehind = new bool[30];
+
+   	//Lists
+    public enum Option {
+        Center = 0,
+        UpperRightCorner = 1,
+        UpperLeftCorner = 2,
+        BottomRightCorner = 3,
+        BottomLeftCorner = 4
+    }
+    public Option opt;
+
+    public enum BoxOption {
+        Center = 0,
+		AboveTheAllComponents=1
+    }
+    public BoxOption boxopt;
+
+    public enum ListOfDo {
+        ChangeLevel = 0,
+        ExitApplication = 1,
+		ChangePage = 2,
+		ChangeQuality = 3
+    }
+	public ListOfDo[] btnDo = new ListOfDo[30];
+
+
+	public enum TexturePlacement {
+		NextToTheButtons = 0
+	}
+	public TexturePlacement textureplacement;
+
+	public enum TextureNearButtonPlacement {
+		Left = 0,
+		Right = 1,
+		Up = 2,
+		Down = 3,
+		Inside = 4
+	}
+	public int[] NearButtonTextureNum = new int[30];
+	public TextureNearButtonPlacement[] nearbtnplacement = new TextureNearButtonPlacement[20];
+
+	//Preview Texture settings
+	public enum PreviewTexturePlacement {
+		LeftSide = 0,
+		RightSide = 1,
+		MiddleOfTheTop = 2,
+		MidBottom = 3,
+		UpperRightCorner=4,
+		UpperLeftCorner=5,
+		BottomRightCorner=6,
+		BottomLeftCorner=7,
+		AboveTheHighestButton=8
+	}
+	public PreviewTexturePlacement previewplacement;
+	public Texture2D previewtexture;
+	public bool UsePreviewTexture_Interface = false;
+	public bool UsePreviewTexture = false;
+	public int widthPreviewTexture = 0;
+	public int heightPreviewTexture = 0;
+
+    //Boxes settings
+    public int CurBoxes;
+	public Rect[] RectsBoxes = new Rect[30];
+	public int[] XposBoxes = new int[30];
+	public int[] YposBoxes = new int[30];
+	public int[] WidthBoxes = new int[30];
+	public int[] HeightBoxes = new int[30];
+	public string[] textOfBoxes = new string[30];
+	public bool UseCustomPosBox;
+	private int minX, minY, maxY, maxX, AllWidth, AllHeight;
+	public string[] boxName = new string[30];
+
+	//Animation settings
+	public float _transparency = 0f;
+	public bool UseStartAnimation = false;
+	public float speedAnimation = 0f;
+	public bool StartAnim_Interface = false;
+
+	void Update() {
+		for (int i = 0; i < 10; ++i) {
+			if (!UseCustomPlusPositions [i]) {
+				PlusX [i] = 0;
+				PlusY [i] = 0;
+			}
+		}
+	}
+
+	void FixedUpdate() {
+		if (UseStartAnimation) {
+			if (_transparency < Transparency)
+				_transparency += speedAnimation;
+		}
+	}
+
+    void OnGUI() {
+		if(guiSkin != null)
+			GUI.skin = guiSkin;
+		
+		if (UseCustomPosBox) {
+			for (int i = 0; i < CurBoxes; ++i) {
+				RectsBoxes [i] = new Rect (XposBoxes [i], YposBoxes [i], WidthBoxes [i], HeightBoxes [i]);
+			}
+		} else {
+			switch (boxopt) {
+			case (BoxOption.AboveTheAllComponents):
+				maxY = -10000; minY = 10000; maxX = -10000; minX = 10000; AllWidth = -10000;
+				for (int L = 0; L < CurButtons; L++) {
+					if (CurPage == startbtnpage [L]) {
+						if (maxY < RectsOfButtons [L].y + RectsOfButtons [L].height)
+							maxY = (int)(RectsOfButtons [L].y + RectsOfButtons [L].height);
+						if (minY > RectsOfButtons [L].y)
+							minY = (int)RectsOfButtons [L].y;
+						if (maxX < RectsOfButtons [L].x + RectsOfButtons [L].width)
+							maxX = (int)(RectsOfButtons [L].x + RectsOfButtons [L].width);
+						if (minX > RectsOfButtons [L].x)
+							minX = (int)RectsOfButtons [L].x;
+						if (AllWidth < (int)RectsOfButtons [L].width)
+							AllWidth = (int)RectsOfButtons [L].width;
+					}
+
+				}
+				break;
+
+			case (BoxOption.Center):
+				for (int i = 1; i < CurBoxes + 1; ++i) {
+					int xBox = Screen.width / 2 - WidthBoxes [i] / 2;
+					int yBox = 0;
+
+					if (i > 0)
+						yBox = (int)RectsBoxes [i - 1].y + (int)RectsBoxes [i - 1].height;
+					else
+						yBox = Screen.height / 2 - height [i] / 2;
+				
+					RectsBoxes [0] = new Rect (Screen.width / 2 - WidthBoxes [0] / 2, Screen.height / 2 - HeightBoxes [0] / 2, WidthBoxes [0], HeightBoxes [0]);
+					RectsBoxes [i] = new Rect (xBox, yBox, WidthBoxes [i], HeightBoxes [i]);
+				}
+
+				break;
+			}
+		}
+	
+		if(UseCustomPosition) {
+			for (int i = 0; i < CurButtons; ++i) {
+				RectsOfButtons[i] = new Rect(xPos[i], yPos[i], width[i], height[i]);
+			}
+		} else {
+			switch(opt) {
+			case(Option.Center):
+				for(int i = 1; i < CurButtons+1; ++i) {
+					int Xbtn = Screen.width/2 - width[i]/2;
+					int yBtn = 0; 
+
+					if(i > 0) yBtn = (int)RectsOfButtons[i-1].y + (int)RectsOfButtons[i-1].height;
+					else yBtn = Screen.height/4 - height[i]/4;
+
+					RectsOfButtons[0] = new Rect(Screen.width/2 - width[0]/2+PlusX[0], Screen.height/4 - height[0]/4+PlusY[0], width[0], height[0]);
+					RectsOfButtons[i] = new Rect(Xbtn + PlusX[i], yBtn + PlusY[i], width[i], height[i]);
+				}
+				break;
+
+			case(Option.UpperLeftCorner) :
+				for(int i = 0; i < CurButtons; ++i) {
+					if(i == 0)RectsOfButtons[i] = new Rect(PlusX[i], PlusY[i]+30, width[i], height[i]);
+					else RectsOfButtons[i] = new Rect(PlusX[i],(int)RectsOfButtons[i-1].y+(int)RectsOfButtons[i-1].height+PlusY[i],width[i], height[i]);
+				}
+				break;
+
+			case(Option.UpperRightCorner) :
+				for (int i = 0; i < CurButtons; ++i) {
+					if(i==0) RectsOfButtons[i] = new Rect(Screen.width-width[i]+PlusX[i], PlusY[i]+30, width[i], height[i]);
+					else RectsOfButtons[i] = new Rect(Screen.width-width[i]+PlusX[i], 
+						(int)RectsOfButtons[i-1].y+(int)RectsOfButtons[i-1].height+PlusY[i], width[i], height[i]);
+					}
+
+				break;
+
+			case (Option.BottomLeftCorner) :
+				for (int i = 0; i < CurButtons; ++i){
+					if (i == 0) RectsOfButtons[i] = new Rect(PlusX[i], Screen.height - height[i] + PlusY[i], width[i], height[i]);
+					else RectsOfButtons[i] = new Rect(PlusX[i], RectsOfButtons[i - 1].y - height[i]+PlusY[i], width[i], height[i]);
+            	}
+				break;
+
+			case(Option.BottomRightCorner) :
+				for (int i = 0; i < CurButtons; ++i) {
+					if(i==0) RectsOfButtons[i] = new Rect(Screen.width-width[i]+PlusX[i], Screen.height - height[i]+PlusY[i], width[i], height[i]);
+					else RectsOfButtons[i] = new Rect(Screen.width-width[i]+PlusX[i], RectsOfButtons[i - 1].y - height[i] + PlusY[i], width[i], height[i]);
+				}
+
+				break;
+			}
+		}
+
+		if (UseStartAnimation)
+			GUI.color = new Color (ColorText.r, ColorText.g, ColorText.b, _transparency / 1000);
+		else
+			GUI.color = new Color (ColorText.r, ColorText.g, ColorText.b, Transparency);
+
+		if (!UseCustomPosBox && boxopt == BoxOption.AboveTheAllComponents) {
+			GUI.Box (new Rect (minX - 5, minY - 30, maxX - minX + 10, maxY - minY + 40), boxName [CurPage]);
+		}
+		else {
+			for (int i = 0; i < CurBoxes; ++i) {
+				GUI.Box (RectsBoxes[i], textOfBoxes[i]);
+			}
+		}
+
+        for (int i = 0; i < CurButtons; ++i) {
+			if (startbtnpage [i] == CurPage) {
+				if (GUI.Button (RectsOfButtons [i], labelsOfButtons [i])) {
+					switch (btnDo [i]) {
+					case ListOfDo.ChangeLevel:
+						SceneManager.LoadScene (SceneNumbers [i]);
+						break;
+					case ListOfDo.ExitApplication:
+						Application.Quit ();
+						break;
+					case ListOfDo.ChangePage:
+						CurPage = changebtnpage [i];
+						break;
+					case ListOfDo.ChangeQuality:
+						QualitySettings.SetQualityLevel (qualitynum[i]);
+						break;
+					}
+
+					if (IsPlaySound [i] && PlaySound[i] != null) {
+						AudioSource.PlayClipAtPoint (PlaySound[i], transform.position);
+					}
+				}
+			}
+        }
+
+		GUI.color = Color.white;
+
+		for (int i = 0; i < TextureCount; ++i) {
+			if (UseCustomPositionTexture) {
+				if (UseBoxBehind[i])
+					GUI.Box (new Rect (XpositionTexture [i], YpositionTexture [i], WidthTexture [i], HeightTexture [i]), "");
+				
+				GUI.DrawTexture (new Rect (XpositionTexture [i], YpositionTexture [i], WidthTexture [i], HeightTexture [i]), Textures [i]);
+			} else {
+				if(Textures[i] != null) {
+				switch (nearbtnplacement[i]) {
+					case TextureNearButtonPlacement.Inside:
+						if(UseBoxBehind[i])
+							GUI.Box (new Rect (RectsOfButtons [NearButtonTextureNum [i] - 1].x, RectsOfButtons [NearButtonTextureNum [i] - 1].y, 
+								RectsOfButtons [NearButtonTextureNum [i] - 1].width, RectsOfButtons [NearButtonTextureNum [i] - 1].height), "");
+						
+						GUI.DrawTexture (new Rect (RectsOfButtons [NearButtonTextureNum [i] - 1].x, RectsOfButtons [NearButtonTextureNum [i] - 1].y, 
+							RectsOfButtons [NearButtonTextureNum [i] - 1].width, RectsOfButtons [NearButtonTextureNum [i] - 1].height), Textures [i]);
+						break;
+
+					case TextureNearButtonPlacement.Right:
+						if(UseBoxBehind[i])
+							GUI.Box (new Rect (RectsOfButtons[NearButtonTextureNum[i]-1].x+RectsOfButtons[NearButtonTextureNum[i]-1].width, 
+								RectsOfButtons[NearButtonTextureNum[i]-1].y, WidthTexture[i], HeightTexture[i]), "");
+
+						GUI.DrawTexture (new Rect (RectsOfButtons[NearButtonTextureNum[i]-1].x+RectsOfButtons[NearButtonTextureNum[i]-1].width, 
+							RectsOfButtons[NearButtonTextureNum[i]-1].y, WidthTexture[i], HeightTexture[i]) , Textures[i]);
+						break;
+					case TextureNearButtonPlacement.Left:
+						if(UseBoxBehind[i])
+							GUI.Box (new Rect (RectsOfButtons[NearButtonTextureNum[i]-1].x-WidthTexture[i], 
+								RectsOfButtons[NearButtonTextureNum[i]-1].y, WidthTexture[i], HeightTexture[i]) , "");
+
+						GUI.DrawTexture (new Rect (RectsOfButtons[NearButtonTextureNum[i]-1].x-WidthTexture[i], 
+							RectsOfButtons[NearButtonTextureNum[i]-1].y, WidthTexture[i], HeightTexture[i]) , Textures[i]);
+						break;
+					case TextureNearButtonPlacement.Up:
+						if(UseBoxBehind[i])
+							GUI.Box (new Rect (RectsOfButtons[NearButtonTextureNum[i]-1].x, 
+								RectsOfButtons[NearButtonTextureNum[i]-1].y-HeightTexture[i], WidthTexture[i], HeightTexture[i]) , "");
+						
+						GUI.DrawTexture (new Rect (RectsOfButtons[NearButtonTextureNum[i]-1].x, 
+							RectsOfButtons[NearButtonTextureNum[i]-1].y-HeightTexture[i], WidthTexture[i], HeightTexture[i]) , Textures[i]);
+						break;
+					case TextureNearButtonPlacement.Down:
+						if(UseBoxBehind[i])
+							GUI.Box (new Rect (RectsOfButtons[NearButtonTextureNum[i]-1].x, 
+								RectsOfButtons[NearButtonTextureNum[i]-1].y+RectsOfButtons[NearButtonTextureNum[i]-1].height, WidthTexture[i], HeightTexture[i]) , "");
+						
+						GUI.DrawTexture (new Rect (RectsOfButtons[NearButtonTextureNum[i]-1].x, 
+							RectsOfButtons[NearButtonTextureNum[i]-1].y+RectsOfButtons[NearButtonTextureNum[i]-1].height, WidthTexture[i], HeightTexture[i]) , Textures[i]);
+						break;
+					}
+				}
+			}
+		}
+		if (UsePreviewTexture) {
+			switch (previewplacement) {
+			case PreviewTexturePlacement.MiddleOfTheTop:
+				GUI.DrawTexture (new Rect (Screen.width / 2 - widthPreviewTexture / 2, 0, widthPreviewTexture, heightPreviewTexture), previewtexture);
+				break;
+			case PreviewTexturePlacement.MidBottom:
+				GUI.DrawTexture (new Rect (Screen.width / 2 - widthPreviewTexture / 2, Screen.height - heightPreviewTexture, widthPreviewTexture, heightPreviewTexture), previewtexture);
+				break;
+			case PreviewTexturePlacement.LeftSide:
+				GUI.DrawTexture (new Rect (0, Screen.height / 2 - heightPreviewTexture / 2, widthPreviewTexture, heightPreviewTexture), previewtexture);
+				break;
+			case PreviewTexturePlacement.RightSide:
+				GUI.DrawTexture (new Rect (Screen.width - widthPreviewTexture, Screen.height / 2 - heightPreviewTexture / 2, widthPreviewTexture, heightPreviewTexture), previewtexture);
+				break;
+			case PreviewTexturePlacement.UpperLeftCorner:
+				GUI.DrawTexture (new Rect (0, 0, widthPreviewTexture, heightPreviewTexture), previewtexture);
+				break;
+			case PreviewTexturePlacement.UpperRightCorner:
+				GUI.DrawTexture (new Rect (Screen.width - widthPreviewTexture, 0, widthPreviewTexture, heightPreviewTexture), previewtexture);
+				break;
+			case PreviewTexturePlacement.BottomLeftCorner:
+				GUI.DrawTexture (new Rect (0, Screen.height - heightPreviewTexture, widthPreviewTexture, heightPreviewTexture), previewtexture);
+				break;
+			case PreviewTexturePlacement.BottomRightCorner:
+				GUI.DrawTexture (new Rect (Screen.width - widthPreviewTexture, Screen.height - heightPreviewTexture, widthPreviewTexture, heightPreviewTexture), previewtexture);
+				break;
+			case PreviewTexturePlacement.AboveTheHighestButton:
+				float minY_p = 100000;
+				float minX_p = 100000;
+				float maxX_p = -100000;
+				for (int i = 0; i < CurButtons; ++i) {
+					if (minY_p > RectsOfButtons [i].y)
+						minY_p = RectsOfButtons [i].y;
+					if (minX_p > RectsOfButtons [i].x)
+						minX_p = RectsOfButtons [i].x;
+					if (maxX_p < RectsOfButtons [i].x)
+						maxX_p = RectsOfButtons [i].x+RectsOfButtons[i].width;
+				}
+
+				GUI.DrawTexture (new Rect ((int)(maxX_p + minX_p) / 2 - widthPreviewTexture/2, (int)minY_p - heightPreviewTexture-30, widthPreviewTexture, heightPreviewTexture), previewtexture); 
+				break;
+			}
+		}
+	}
+}
